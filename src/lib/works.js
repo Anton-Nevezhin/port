1a832453cae44558e4db03cc32f1d8a776cb60a9
anton-nevezhin <anton-nevezhin@yandex.ru>
2023-01-18 22:11:25 +0300

    twentieth commit

--- src/lib/works.js
+++ src/lib/works.js
@@ -1,40 +1,100 @@
-// const foto = ['1.jpg', '2.jpg', '3.jpg']
-const myFirstEvent = document.getElementById('works__right')
-let s
+document.getElementById('outer3').addEventListener('click', toggleState3)
 
-// let i = 0
-let j = 0
-let k
-// let l
-const image = document.getElementById('works__slider')
-const image1 = document.getElementById('works__slider1')
-// const text = document.getElementById('text')
-
-function next () {
-//  i++
-  j++
-
-  if (j === 3) {
-    j = 0
-  }
-
-  k = 800 - j * 400
-  s = k + 'px'
-  image.style.marginLeft = s
-
-  if (j === 1) {
-    image1.style.width = '750px'
-    image1.style.height = '500px'
+function toggleState3 () {
+  let galleryView = document.getElementById('galleryView')
+  let tilesView = document.getElementById('tilesView')
+  let outer = document.getElementById('outer3')
+  let slider = document.getElementById('slider3')
+  let tilesContainer = document.getElementById('tilesContainer')
+  if (slider.classList.contains('active')) {
+    slider.classList.remove('active')
+    outer.classList.remove('outerActive')
+    galleryView.style.display = 'flex'
+    tilesView.style.display = 'none'
+    while (tilesContainer.hasChildNodes()) {
+      tilesContainer.removeChild(tilesContainer.firstChild)
+    }
   } else {
-    image1.style.width = '450px'
-    image1.style.height = '300px'
-  }
+    slider.classList.add('active')
+    outer.classList.add('outerActive')
+    galleryView.style.display = 'none'
+    tilesView.style.display = 'flex'
 
-  k = 800 - j * 400
-  s = k + 'px'
-  image.style.marginLeft = s
+    for (let i = 0; i < imgObject.length - 1; i++) {
+      let tileItem = document.createElement('div')
+      tileItem.classList.add('tileItem')
+      tileItem.style.background = 'url(" + imgObject[i] + ")'
+      tileItem.style.backgroundSize = 'contain'
+      tilesContainer.appendChild(tileItem)
+    }
+  };
 }
 
-myFirstEvent.addEventListener('click', next)
+let imgObject = [
+  'https://placeimg.com/450/450/any',
+  'https://placeimg.com/450/450/animals',
+  'https://placeimg.com/450/450/architecture',
+  'https://placeimg.com/450/450/nature',
+  'https://placeimg.com/450/450/people',
+  'https://placeimg.com/450/450/tech',
+  'https://picsum.photos/id/1/450/450',
+  'https://picsum.photos/id/8/450/450',
+  'https://picsum.photos/id/12/450/450',
+  'https://picsum.photos/id/15/450/450',
+  'https://picsum.photos/id/5/450/450'
+]
 
-next()
+let mainImg = 0
+let prevImg = imgObject.length - 1
+let nextImg = 1
+
+function loadGallery () {
+  let mainView = document.getElementById('mainView')
+  mainView.style.background = 'url(" + imgObject[mainImg] + ")'
+
+  let leftView = document.getElementById('leftView')
+  leftView.style.background = 'url(" + imgObject[prevImg] + ")'
+
+  let rightView = document.getElementById('rightView')
+  rightView.style.background = 'url(" + imgObject[nextImg] + ")'
+
+  let linkTag = document.getElementById('linkTag')
+  linkTag.href = imgObject[mainImg]
+};
+
+function scrollRight () {
+  prevImg = mainImg
+  mainImg = nextImg
+  if (nextImg >= (imgObject.length - 1)) {
+    nextImg = 0
+  } else {
+    nextImg++
+  }
+  loadGallery()
+};
+
+function scrollLeft () {
+  nextImg = mainImg
+  mainImg = prevImg
+
+  if (prevImg === 0) {
+    prevImg = imgObject.length - 1
+  } else {
+    prevImg--
+  };
+  loadGallery()
+};
+
+document.getElementById('navRight').addEventListener('click', scrollRight)
+document.getElementById('navLeft').addEventListener('click', scrollLeft)
+document.getElementById('rightView').addEventListener('click', scrollRight)
+document.getElementById('leftView').addEventListener('click', scrollLeft)
+document.addEventListener('keyup', function (e) {
+  if (e.keyCode === 37) {
+    scrollLeft()
+  } else if (e.keyCode === 39) {
+    scrollRight()
+  }
+})
+
+loadGallery()
